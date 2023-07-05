1856850b9baac3fab11a3f967dbf961bc3617247
pravin gaikwad <psgaikwad312@yahoo.com>
2023-07-05 11:14:28 +0530

    Get tow numbers.

--- sample.cpp
+++ sample.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+	int a=0,b=0;
 	std::cout<<"Test git"<<std::endl;
+	std::cout<<"Enter first number"<<std::endl;
+	std::cin>>a;
+	std::cout<<"Enter second number"<<std::endl;
+	std::cin>>b;
+	std::cout<<"Sum of a+b="<<a+b<<std::endl;
 	return 0;
 }
